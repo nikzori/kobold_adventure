179a488d1cd84dc93b891b3e2fb4a24738d6edfd
nikzori <kzr.niki@gmail.com>
2024-07-04 23:13:32 +0300

    added tilesheet

--- Scripts/Resourses/Creature.cs
+++ Scripts/Resourses/Creature.cs
@@ -4,12 +4,25 @@ public partial class Creature : Resource
 {
     public string name;
 
+    [Export(hintString: "Vigor")]
+    public int Vigor;
+    [Export(hintString: "Reflex")]
+    public int Reflex;
+    [Export(hintString: "Cognition")]
+    public int Cognition;
+    [Export(hintString: "Willpower")]
+    public int Willpower;
+
     private int _health;
     public int Health {get {return _health;} set {_health = value;}}
     public int armor;
     public void ApplyDamage(int damage)
     {
         //some fancy damage reduction formula here later
-        Health -= damage;
+        int value = damage - armor;
+        Health -= value > 0 ? value : 1;
     }
+
+    [Export(hintString: "Damage")]
+    public int damage;
 }
